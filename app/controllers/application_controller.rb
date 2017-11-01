e8a55a6f0b301b6995ba32ccd91f99ddc218dd95
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-01 09:53:33 -0700

    Add log in and log out links to header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :find_user
+
+  private
+  def find_user
+    @user = User.find_by(id: session[:user_id])
+  end
 end
