754b954b36f109bee4c99fd1b00d1549b1faf52b
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-05 10:48:12 -0800

    Add dependencies to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+require_dependency '../../lib/api_wrapper'
+require_dependency '../../lib/recipe'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :find_user
