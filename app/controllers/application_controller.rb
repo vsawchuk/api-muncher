38c2332e345e988759b4130c85978dbc7e98ffbd
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-05 11:02:31 -0800

    Comment out dependencies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-require_dependency '../../lib/api_wrapper'
-require_dependency '../../lib/recipe'
+# require_dependency '../../lib/api_wrapper'
+# require_dependency '../../lib/recipe'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
